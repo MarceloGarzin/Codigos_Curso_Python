2b78c239a32c1addc45731b41d82bb3da61bd1c3
MarceloGarzin <marcelogarzins@hotmail.com.br>
2024-02-27 15:24:42 -0300

    Create Exercício_9_.py
     Crie uma função que multiplica todos os argumentos
     não nomeados recebidos
     Retorne o total para uma variável e mostre o valor da variável.

---
+++ Exercício_9_.py
@@ -0,0 +1,34 @@
+# Exercícios com funções
+
+# Crie uma função que multiplica todos os argumentos
+# não nomeados recebidos
+# Retorne o total para uma variável e mostre o valor
+# da variável.
+
+def multiplicar(*args):
+    total = 1
+    for numero in args:
+        total *= numero
+    return total
+
+multiplicacao = multiplicar(1,2,3,4)    
+
+def par_impar(numero):
+    multiplo_de_dois = numero % 2 == 0
+
+    if multiplo_de_dois:
+        return f'{numero} é par'
+    return f'{numero} é impar'
+
+
+print(par_impar(multiplicacao))
+
+
+          
+
+
+
+
+
+
+
